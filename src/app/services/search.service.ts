aa83ce66c977acd42c866aee52f632c45aad7a19
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2024-06-02 18:22:22 +0300

    update

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@angular/core';
+import { title } from 'node:process';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SearchService {
+  adds = [
+    {
+      title: 'רנט איט מוריה אטריום',
+      pic: 'assets/images/ad2.jpg',
+      city: 'מודעין מכבים רעות',
+      program: 'שכירות ארוכת טווח',
+    },
+
+    {
+      title: 'אשטרום תל אביב',
+      pic: 'assets/images/ad1.jpg',
+      city: 'תל אביב יפו',
+      program: 'מהפכת השכירות בתל אביב',
+    },
+    {
+      title: 'מורשת טל',
+      pic: 'assets/images/ad3.jpg',
+      city: 'מודעין מכבים רעות',
+      program: 'מבצע חד-פעמי לפנטהואוזים!',
+    },
+  ];
   selectedPropertyTypes: BehaviorSubject<string[]> = new BehaviorSubject<
     string[]
   >([]);
