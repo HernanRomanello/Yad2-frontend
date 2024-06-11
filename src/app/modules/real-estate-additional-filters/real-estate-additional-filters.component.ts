ec7f710509e0e8f7496159f90e1f182a790989e6
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2024-06-11 11:21:13 +0300

    update

--- src/app/modules/real-estate-additional-filters/real-estate-additional-filters.component.ts
+++ src/app/modules/real-estate-additional-filters/real-estate-additional-filters.component.ts
@@ -3,8 +3,79 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-real-estate-additional-filters',
   templateUrl: './real-estate-additional-filters.component.html',
-  styleUrl: './real-estate-additional-filters.component.css'
+  styleUrl: './real-estate-additional-filters.component.css',
 })
 export class RealEstateAdditionalFiltersComponent {
+  filters = {
+    withImage: false,
+    onlyCommunities: false,
+    withPrice: false,
+    priceDropped: false,
+    newProperty: false,
+    publisher: '',
+    parking: false,
+    elevator: false,
+    mamad: false,
+    balcony: false,
+    airConditioning: false,
+    storage: false,
+    renovated: false,
+    accessible: false,
+    bars: false,
+    furnished: false,
+    exclusive: false,
+    newFromContractor: false,
+    new: false,
+    renovated5Years: false,
+    wellMaintained: false,
+    needsRenovation: false,
+    basement: false,
+    floorMin: null,
+    floorMax: null,
+    sizeMin: null,
+    sizeMax: null,
+    entryDate: '',
+    immediateEntry: false,
+    freeSearch: '',
+  };
 
+  resetFilters() {
+    this.filters = {
+      withImage: false,
+      onlyCommunities: false,
+      withPrice: false,
+      priceDropped: false,
+      newProperty: false,
+      publisher: '',
+      parking: false,
+      elevator: false,
+      mamad: false,
+      balcony: false,
+      airConditioning: false,
+      storage: false,
+      renovated: false,
+      accessible: false,
+      bars: false,
+      furnished: false,
+      exclusive: false,
+      newFromContractor: false,
+      new: false,
+      renovated5Years: false,
+      wellMaintained: false,
+      needsRenovation: false,
+      basement: false,
+      floorMin: null,
+      floorMax: null,
+      sizeMin: null,
+      sizeMax: null,
+      entryDate: '',
+      immediateEntry: false,
+      freeSearch: '',
+    };
+  }
+
+  applyFilters() {
+    console.log(this.filters);
+    // Implement filter logic here
+  }
 }
