ca6126cacedfe05783a27a4253e0025cbdc50a00
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2024-08-15 01:11:28 +0300

    update

--- src/app/modules/post-ad-confirmation-modal/post-ad-confirmation-modal.component.ts
+++ src/app/modules/post-ad-confirmation-modal/post-ad-confirmation-modal.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../../services/user/auth.service';
 
 @Component({
   selector: 'app-post-ad-confirmation-modal',
   templateUrl: './post-ad-confirmation-modal.component.html',
-  styleUrl: './post-ad-confirmation-modal.component.css'
+  styleUrl: './post-ad-confirmation-modal.component.css',
 })
-export class PostAdConfirmationModalComponent {
+export class PostAdConfirmationModalComponent implements OnInit, OnDestroy {
+  constructor(private authService: AuthService) {}
+  ngOnDestroy(): void {
+    this.authService.IsalternativeHeaderISOpen.next(false);
+  }
 
+  ngOnInit() {
+    this.authService.IsalternativeHeaderISOpen.next(true);
+  }
 }
