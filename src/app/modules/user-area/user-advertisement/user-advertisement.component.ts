5b073d35b16432f1154f17eff6065e7524fab7ad
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2024-08-30 07:38:25 +0300

    update

--- src/app/modules/user-area/user-advertisement/user-advertisement.component.ts
+++ src/app/modules/user-area/user-advertisement/user-advertisement.component.ts
@@ -1,4 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { AuthService } from '../../../services/user/auth.service';
 import { AdvertisementService } from '../../../services/advertisement.service';
 
@@ -8,24 +14,26 @@ import { AdvertisementService } from '../../../services/advertisement.service';
   styleUrl: './user-advertisement.component.css',
 })
 export class UserAdvertisementComponent implements OnInit {
+  authService = inject(AuthService);
+  advertisementService = inject(AdvertisementService);
+  assetTypes: string[] = [];
+  currentSlidesData: { [key: number]: { adIndex: number; id: number } } = {};
+  userAdvertisements = this.authService.UserAdvertisements;
+
   ngOnInit(): void {
     this.authService.UserAdvertisements.subscribe((ads) => {
       ads.forEach((value, index) => {
         if (index > 2) {
-          this.currentSlides[index] = 0;
-          this.currentSlidesId[index] = value.id;
+          this.currentSlidesData[index] = { adIndex: 0, id: value.id };
         } else {
-          this.currentSlides[index] = 2;
+          this.currentSlidesData[index] = { adIndex: 2, id: value.id };
         }
       });
     });
+    console.log('Current Slides Data:', this.currentSlidesData); // Verify the data
+
+    console.log('index   ' + this.findAdIndexById(20));
   }
-  authService = inject(AuthService);
-  advertisementService = inject(AdvertisementService);
-  assetTypes: string[] = [];
-  currentSlides: { [adIndex: number]: number } = {};
-  currentSlidesId: { [id: number]: number } = {};
-  userAdvertisements = this.authService.UserAdvertisements;
 
   setAssetType(option: any) {}
 
@@ -33,19 +41,26 @@ export class UserAdvertisementComponent implements OnInit {
 
   toggleDropdown(text: string) {}
 
-  currentSlide = 0;
-
-  goToSlide(increment: boolean, decrement: boolean, totalSlides: number) {
-    if (increment) {
-      this.currentSlide++;
-      if (this.currentSlide + 1 >= totalSlides) {
-        this.currentSlide = totalSlides - 1;
-      }
-    } else if (decrement) {
-      this.currentSlide--;
-      if (this.currentSlide < 0) {
-        this.currentSlide = 0;
+  findAdIndexById(id: number): number {
+    for (const key in this.currentSlidesData) {
+      if (this.currentSlidesData[key].id === id) {
+        return this.currentSlidesData[key].adIndex; // Return the adIndex of the matched object
       }
     }
+    return -1; // Return -1 if the id is not found
+  }
+
+  goToSlide(increment: boolean, decrement: boolean, totalSlides: number) {
+    // if (increment) {
+    //   this.currentSlide++;
+    //   if (this.currentSlide + 1 >= totalSlides) {
+    //     this.currentSlide = totalSlides - 1;
+    //   }
+    // } else if (decrement) {
+    //   this.currentSlide--;
+    //   if (this.currentSlide < 0) {
+    //     this.currentSlide = 0;
+    //   }
+    // }
   }
 }
