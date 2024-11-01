add6ecccdf00d681c72840e33347ddc079209054
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2024-11-01 10:35:06 +0200

    1.11 10:35

--- src/app/modules/user-area/upload-profile-image/upload-profile-image.component.ts
+++ src/app/modules/user-area/upload-profile-image/upload-profile-image.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-upload-profile-image',
   templateUrl: './upload-profile-image.component.html',
-  styleUrl: './upload-profile-image.component.css'
+  styleUrl: './upload-profile-image.component.css',
 })
 export class UploadProfileImageComponent {
-
+  isModalVisible = true;
 }
