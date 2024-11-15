96677c4466e3630d1e8475481a31da0b1f6a9d4e
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2024-11-15 13:15:04 +0200

    15.11 13:15

--- src/app/modules/favorite-ads/favorite-ads.component.ts
+++ src/app/modules/favorite-ads/favorite-ads.component.ts
@@ -17,6 +17,24 @@ export class FavoriteAdsComponent implements OnInit, OnDestroy {
     this.authService.isFavoritesAdIsOpen(false);
   }
 
+  displayAdOptions(event: any, index: number) {
+    let divTarget = document.getElementById(
+      'ad-date-update' + index.toString()
+    );
+
+    if (event.type === 'mouseenter' && divTarget) {
+      divTarget.innerHTML = 'לחצו לפרטים';
+      this.renderer.removeClass(divTarget, 'decrease-opacity');
+      this.renderer.addClass(divTarget, 'orange-font');
+      this.renderer.addClass(divTarget, 'font500');
+    } else if (event.type === 'mouseleave' && divTarget) {
+      this.renderer.removeClass(divTarget, 'orange-font');
+      this.renderer.removeClass(divTarget, 'font500');
+
+      divTarget.innerHTML = 'עודכן היום';
+    }
+  }
+
   zoomInImage(event: any, index: number) {
     let divTarget = document.getElementById('ad-image' + index.toString());
 
