d02f0a6f55693c14d920e65bc4084849afc7e11d
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2024-12-03 09:21:31 +0200

    3.12 9:21

--- src/app/modules/user-area/statistic/statistic.component.ts
+++ src/app/modules/user-area/statistic/statistic.component.ts
@@ -9,6 +9,13 @@ export class StatisticComponent {
   watchedAdisChecked = true;
   saveAdisChecked = true;
   clickedAdisChecked = true;
-  dropDownValue = '7 ימים אחרונים';
   vaerticalLineIsVisible = false;
+  dropDownOptions: string[] = [
+    '7 ימים אחרונים',
+    '14 ימים אחרונים',
+    '30 ימים אחרונים',
+    '90 ימים אחרונים',
+  ];
+  dropDownValue = this.dropDownOptions[0];
+  dropDownIsVisible = true;
 }
