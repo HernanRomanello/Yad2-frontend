ed890a89d65a4d2ae74f2907ae4c33ed42bae150
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2024-12-13 17:35:42 +0200

    13.12 17:35

--- src/app/modules/user-area/last-searches/last-searches.component.ts
+++ src/app/modules/user-area/last-searches/last-searches.component.ts
@@ -24,6 +24,37 @@ export class LastSearchesComponent implements OnInit, OnDestroy {
     }`;
   }
 
+  transformToHebrewMonth(month: number): string {
+    switch (month) {
+      case 1:
+        return 'ינואר';
+      case 2:
+        return 'פברואר';
+      case 3:
+        return 'מרץ';
+      case 4:
+        return 'אפריל';
+      case 5:
+        return 'מאי';
+      case 6:
+        return 'יוני';
+      case 7:
+        return 'יולי';
+      case 8:
+        return 'אוגוסט';
+      case 9:
+        return 'ספטמבר';
+      case 10:
+        return 'אוקטובר';
+      case 11:
+        return 'נובמבר';
+      case 12:
+        return 'דצמבר';
+      default:
+        return '';
+    }
+  }
+
   ngOnDestroy() {
     this.navigationService.isFavoriteAdvertisementOrLastsearchesIsOpen(false);
   }
