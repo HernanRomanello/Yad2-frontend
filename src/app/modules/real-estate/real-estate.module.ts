8f8d58ec875214e6ec6915f1421f496f54673486
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2026-01-30 21:08:31 +0200

    refactor results mobile

--- src/app/modules/real-estate/real-estate.module.ts
+++ src/app/modules/real-estate/real-estate.module.ts
@@ -14,6 +14,7 @@ import { RealEstateAdditionalFiltersComponent } from '../real-estate-additional-
 import { RealEstateRoomsAmountComponent } from '../real-estate-rooms-amount/real-estate-rooms-amount.component';
 import { RealEstateFloorSliderSelectComponent } from './real-estate-price-slider-select/real-estate-price-slider-select.component';
 import { RealEstateSquareSizeSliderComponent } from '../real-estate-price-slider-component2/real-estate-price-slider-component2.component';
+import { MobileSearchHeaderComponent } from './real-estate-search/mobile-search-header/mobile-search-header.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { RealEstateSquareSizeSliderComponent } from '../real-estate-price-slider
     RealEstatePropertyTypeComponent,
     RealEstateAdditionalFiltersComponent,
     RealEstateRoomsAmountComponent,
+    MobileSearchHeaderComponent,
   ],
   imports: [
     CommonModule,
